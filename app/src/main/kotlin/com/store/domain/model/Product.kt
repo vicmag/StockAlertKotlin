9caaa6307bead1ba2c9daf63e4dc38c15ccfb2ca
vicmag <vicmag@hotmail.com>
2025-12-08 17:56:40 +0000

    Inicio para ccc

--- app/src/main/kotlin/com/store/domain/model/Product.kt
+++
@@ -1,6 +0,0 @@
-package com.store.domain.model
-
-data class Product(
-    val name: String,
-    var stock: Int
-)
--- app/src/main/kotlin/com/store/domain/port/ProductRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.store.domain.port
-
-import com.store.domain.model.Product
-
-interface ProductRepository{
-    fun findByName(name: String): Product
-    fun save(product: Product)
-}
